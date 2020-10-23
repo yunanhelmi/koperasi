e62113dc1f63a9e1f1cec1d2c1a78a5f38f3e9b0
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 16:38:46 +0700

    Update and rename KodeAkuncon.php to Kodeakuncon.php

--- application/controllers/Kodeakuncon.php
+++ application/controllers/Kodeakuncon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class KodeAkunCon extends CI_Controller {
+class KodeakunCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
