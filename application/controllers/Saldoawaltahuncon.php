98c0c932430eea33be98615814c3297bca9e9668
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 17:08:18 +0700

    Update and rename SaldoAwalTahuncon.php to Saldoawaltahuncon.php

--- application/controllers/Saldoawaltahuncon.php
+++ application/controllers/Saldoawaltahuncon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class SaldoAwalTahunCon extends CI_Controller {
+class SaldoawaltahunCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
