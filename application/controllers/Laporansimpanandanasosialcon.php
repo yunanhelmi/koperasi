1752eb693df03533a1c6746a089cf5b9b75ac2f0
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 16:48:12 +0700

    Update and rename LaporanSimpananDanaSosialcon.php to Laporansimpanandanasosialcon.php

--- application/controllers/Laporansimpanandanasosialcon.php
+++ application/controllers/Laporansimpanandanasosialcon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class LaporanSimpananDanaSosialCon extends CI_Controller {
+class LaporansimpanandanasosialCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
