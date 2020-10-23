39c1a34f241e59c8dfdc2139e926479fca335508
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 16:37:54 +0700

    Update and rename CekLaporanRincianJasacon.php to Ceklaporanrincianjasacon.php

--- application/controllers/Ceklaporanrincianjasacon.php
+++ application/controllers/Ceklaporanrincianjasacon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class CekLaporanRincianJasacon extends CI_Controller {
+class CeklaporanrincianjasaCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
