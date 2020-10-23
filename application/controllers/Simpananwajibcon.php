e0ef0271d46d2cdd4e79ab5175c5ef8e15a59642
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 17:15:25 +0700

    Update Simpananwajibcon.php

--- application/controllers/Simpananwajibcon.php
+++ application/controllers/Simpananwajibcon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Simpananwajibcon extends CI_Controller {
+class SimpananwajibCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model('usermodel');
