df51e7db971d39f3ac69c7203c6c132bd1bf96da
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 17:10:09 +0700

    Update Simpanan3thmastercon.php

--- application/controllers/Simpanan3thmastercon.php
+++ application/controllers/Simpanan3thmastercon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Simpanan3thMastercon extends CI_Controller {
+class Simpanan3thmasterCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model('usermodel');
