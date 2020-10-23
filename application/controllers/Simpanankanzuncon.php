dfa64a77757bee1dfa8f8a3f879dc160fdb1f40d
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 17:11:15 +0700

    Update Simpanankanzuncon.php

--- application/controllers/Simpanankanzuncon.php
+++ application/controllers/Simpanankanzuncon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Simpanankanzuncon extends CI_Controller {
+class SimpanankanzunCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model('usermodel');
