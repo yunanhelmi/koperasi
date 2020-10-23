17cbf92091b82b6ad8d88282adceac16b3013dc4
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 17:05:44 +0700

    Update and rename LaporanSimpananWajibcon.php to Laporansimpananwajibcon.php

--- application/controllers/Laporansimpananwajibcon.php
+++ application/controllers/Laporansimpananwajibcon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class LaporanSimpananWajibCon extends CI_Controller {
+class LaporansimpananwajibCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
