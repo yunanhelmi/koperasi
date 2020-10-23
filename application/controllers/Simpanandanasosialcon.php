0658b19e4cc11096a9b05d782a0473038ac082ac
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 17:10:43 +0700

    Update Simpanandanasosialcon.php

--- application/controllers/Simpanandanasosialcon.php
+++ application/controllers/Simpanandanasosialcon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Simpanandanasosialcon extends CI_Controller {
+class SimpanandanasosialCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model('usermodel');
