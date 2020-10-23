654409bb623bcdea378ba1a8d27dd3d5ed950496
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 16:49:47 +0700

    Update and rename LaporanSimpananKhususcon.php to Laporansimpanankhususcon.php

--- application/controllers/Laporansimpanankhususcon.php
+++ application/controllers/Laporansimpanankhususcon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class LaporanSimpananKhususCon extends CI_Controller {
+class LaporansimpanankhususCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
