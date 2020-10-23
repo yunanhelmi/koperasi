173f7897545de96f5e10514ddaf189e35da8c81d
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 16:43:22 +0700

    Update and rename LaporanRincianJasacon.php to Laporanrincianjasacon.php

--- application/controllers/Laporanrincianjasacon.php
+++ application/controllers/Laporanrincianjasacon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class LaporanRincianJasaCon extends CI_Controller {
+class LaporanrincianjasaCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
