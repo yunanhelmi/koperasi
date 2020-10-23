597f7428841c08585c3510e3bc3967595ec22697
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 17:07:35 +0700

    Update and rename MappingKodeAkuncon.php to Mappingkodeakuncon.php

--- application/controllers/Mappingkodeakuncon.php
+++ application/controllers/Mappingkodeakuncon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class MappingKodeAkunCon extends CI_Controller {
+class MappingkodeakunCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
