870f8cd5ffd990ea76e3fc845d04c7ae892648ff
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 16:46:18 +0700

    Update and rename LaporanSimpanan3thcon.php to Laporansimpanan3thcon.php

--- application/controllers/Laporansimpanan3thcon.php
+++ application/controllers/Laporansimpanan3thcon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class LaporanSimpanan3thCon extends CI_Controller {
+class Laporansimpanan3thCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
