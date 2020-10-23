17eb89a40a14f0dd91e818c7f967faf6e66f7230
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 17:11:58 +0700

    Update Simpanankhususcon.php

--- application/controllers/Simpanankhususcon.php
+++ application/controllers/Simpanankhususcon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Simpanankhususcon extends CI_Controller {
+class SimpanankhususCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model('usermodel');
