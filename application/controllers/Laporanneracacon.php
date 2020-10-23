58bc4b06e5529125ff60567f7ae07b6101e7d52e
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-23 16:41:52 +0700

    Update and rename LaporanNeracacon.php to Laporanneracacon.php

--- application/controllers/Laporanneracacon.php
+++ application/controllers/Laporanneracacon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class LaporanNeracaCon extends CI_Controller {
+class LaporanneracaCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
