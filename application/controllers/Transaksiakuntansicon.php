51ee8508179229155ae4da9ef9767ed85db40e2d
yunanhelmi <yunanhelmimahendra@gmail.com>
2020-10-25 21:12:05 +0700

    Update and rename TransaksiAkuntansicon.php to Transaksiakuntansicon.php

--- application/controllers/Transaksiakuntansicon.php
+++ application/controllers/Transaksiakuntansicon.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class TransaksiAkuntansiCon extends CI_Controller {
+class TransaksiakuntansiCon extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 
