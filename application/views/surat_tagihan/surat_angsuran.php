d586c24096388707ca5a70d8de71030124c6e923
yunanhm <yunanhelmimahendra@gmail.com>
2025-01-16 21:28:40 +0700

    m

--- application/views/surat_tagihan/surat_angsuran.php
+++ application/views/surat_tagihan/surat_angsuran.php
@@ -158,9 +158,9 @@
 <div id="halaman">
 
     <p class="header_content">
-        Nomor<d style="padding-left:3em;" >: <?php echo $data[0]['nomor_koperasi'] ?> / MM / <?php echo $level == 1 ? 'Srt Pemberitahuan.' : 'Srt Tagihan.' ?> / A / <?php echo $keterangan ?> / <?php echo date("Y") ?></d><br/>
+        Nomor<d style="padding-left:3em;" >: <?php echo $data[0]['nomor_koperasi'] ?> / MM / <?php echo $level <= 1 ? 'Srt Pemberitahuan.' : 'Srt Tagihan.' ?> / A / <?php echo $keterangan ?> / <?php echo date("Y") ?></d><br/>
         Lampiran<d style="padding-left:2em;" >: -</d><br/>
-        Perihal<d style="padding-left:3em;" >: <?php echo $level == 1 ? 'Angsuran Pinjaman' : 'Tagihan Pinjaman' ?></d>
+        Perihal<d style="padding-left:3em;" >: <?php echo $level <= 1 ? 'Angsuran Pinjaman' : 'Tagihan Pinjaman' ?></d>
     </p>
     <p class="header_content">
         Kepada<br/>
--- application/views/surat_tagihan/surat_musiman.php
+++ application/views/surat_tagihan/surat_musiman.php
@@ -151,9 +151,9 @@
 </p>
 <div id="halaman">
     <p class="header_content">
-        Nomor<d style="padding-left:3em;" >: <?php echo $data[0]['nomor_koperasi'] ?> / MM / <?php echo $level == 1 ? 'Srt Pemberitahuan.' : 'Srt Tagihan.' ?> / M / <?php echo $keterangan ?> / <?php echo date("Y") ?></d><br/>
+        Nomor<d style="padding-left:3em;" >: <?php echo $data[0]['nomor_koperasi'] ?> / MM / <?php echo $level <= 1 ? 'Srt Pemberitahuan.' : 'Srt Tagihan.' ?> / M / <?php echo $keterangan ?> / <?php echo date("Y") ?></d><br/>
         Lampiran<d style="padding-left:2em;" >: -</d><br/>
-        Perihal<d style="padding-left:3em;" >: <?php echo $level == 1 ? 'Angsuran Pinjaman' : 'Tagihan Pinjaman' ?></d>
+        Perihal<d style="padding-left:3em;" >: <?php echo $level <= 1 ? 'Angsuran Pinjaman' : 'Tagihan Pinjaman' ?></d>
     </p>
     <p class="header_content">
         <br>
