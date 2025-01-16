74bac390c02cfd93a6d55d5a26ba3cd5da669cd1
yunanhm <yunanhelmimahendra@gmail.com>
2025-01-16 21:36:51 +0700

    m

--- application/views/surat_tagihan/surat_angsuran.php
+++ application/views/surat_tagihan/surat_angsuran.php
@@ -160,7 +160,7 @@
     <p class="header_content">
         Nomor<d style="padding-left:3em;" >: <?php echo $data[0]['nomor_koperasi'] ?> / MM / <?php echo $level <= 1 ? 'Srt Pemberitahuan.' : 'Srt Tagihan.' ?> / A / <?php echo $keterangan ?> / <?php echo date("Y") ?></d><br/>
         Lampiran<d style="padding-left:2em;" >: -</d><br/>
-        Perihal<d style="padding-left:3em;" >: <?php echo $level <= 1 ? 'Tagihan Jatuh Tempo Angsuran' : 'Penyelesaian / Pelunasan Pinjaman' ?></d>
+        Perihal<d style="padding-left:3em;" >: <?php echo $level <= 1 ? 'Tagihan Jatuh Tempo Angsuran' : 'Penyelesaian / Pelunasan Pinjaman Jatuh Tempo' ?></d>
     </p>
     <p class="header_content">
         Kepada<br/>
